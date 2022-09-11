bae84692c9a100601a40e6135fad5f11eda257ab
Gabor Kevin Barta <gkevinba@gmail.com>
2022-09-11 22:37:24 +0200

    changed text on main page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lets deploy this React app.
         </a>
       </header>
     </div>
